a3f7a32acc3a4e6d750c25d4f7c8dff3e8651d07
unknown <113531839+Lu-Zetian@users.noreply.github.com>
2022-10-21 00:39:05 +0800

    Add train.py

---
+++ train.py
@@ -0,0 +1,17 @@
+import torch
+from models.gnn import Model
+
+# Hyper parameters
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def train():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
