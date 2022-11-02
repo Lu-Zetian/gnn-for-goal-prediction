acd2442da9ed06466d9315a72d29494ef0ae6f98
unknown <113531839+Lu-Zetian@users.noreply.github.com>
2022-11-02 15:20:22 +0800

    Add train structure

--- train.py
+++ train.py
@@ -1,16 +1,29 @@
 import torch
-from models.gnn import Model
+import torch.nn as nn
+from sports_gnn.model import SportsGNN
 
 # Hyper parameters
+learning_rate = 1e-4
+epochs = 10
+batch_size = 32
+weight_decay = 1e-5
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def train():
-    pass
+def train(model, train_dataloader, loss_fn, optimizer):
+    model.train()
+    # TODO: single training loop
 
 
 def main():
-    pass
+    model = SportsGNN().to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    loss_fn = nn.CrossEntropyLoss()
+    
+    # TODO: get dataloader
+    
+    for epoch in range(epochs):
+        train(model, train_dataloader, loss_fn, optimizer)
 
 
 if __name__ == "__main__":
