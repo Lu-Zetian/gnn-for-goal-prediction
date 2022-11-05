ff07ea074ae480616342cda83747ee1eef984159
BrianLo <66210536+scs2007022@users.noreply.github.com>
2022-11-05 23:22:16 +0800

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -8,21 +8,24 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.width', None)
 
 # heads=100
-df_EURO = sb.matches(competition_id=55,season_id=43)
-# print(df_EURO.columns)
 
-print(df_EURO[['match_id','match_date','home_team','away_team','home_score','away_score','competition_stage','stadium']])
-
-DataLocation = r'C:\Users\scs20\OneDrive - HKUST Connect\2022-23_Fall\COMP4222\Project\comp4222-project\DataDownload'
-
+DataLocation = os.path.dirname(os.path.abspath(__file__))
+# print(DataLocation)
 from DataParsing import DataParsing 
+from DataParsing.DataParsing import Match,GameState
 
 def mp_construct(match_id):
+    df_EURO = sb.matches(competition_id=55,season_id=43)
     e = pd.read_pickle(os.path.join(DataLocation,f'{match_id}_events.pkl'))
     f = pd.read_pickle(os.path.join(DataLocation,f'{match_id}_frames.pkl'))
     m = DataParsing.Match(match_id,match_df=df_EURO,event_df=e,frame_df=f)
     return m
+
 def parsing():
+    df_EURO = sb.matches(competition_id=55,season_id=43)
+    print(df_EURO.columns)
+
+    print(df_EURO[['match_id','match_date','home_team','away_team','home_score','away_score','competition_stage','stadium']])
     print('start')
     print(len(df_EURO['match_id']))
     with mp.Pool() as pool:
@@ -38,8 +41,9 @@ def parsing():
 import pickle,time
 st = time.time()
 # objects = []
-os.chdir(os.path.dirname(os.path.abspath(__name__)))
-with (open('data_2.pickle','rb')) as openfile:  # read the parsed data
+# with (open('data_2.pickle','rb')) as openfile:  # read the parsed data
+sample = os.path.join(DataLocation,'sample_data.pkl')
+with (open(sample,'rb')) as openfile:  # read the parsed data
     print('reading')
     while True:
         try:
@@ -48,15 +52,27 @@ with (open('data_2.pickle','rb')) as openfile:  # read the parsed data
             break
     print('loaded')
 et = time.time()
-print(et-st) #loading time
+print('Reading time:\t',et-st) #loading time
 
+print('-'*60)
 print('match:\t',data[0].match_id)
+print('-'*60)
+
 print('Label:\t',data[0].gamestates[0].label)
 print('Graph:\t',data[0].gamestates[0].graph)
 print('Meta: \t',data[0].gamestates[0].metadata)        
+print('-'*60)
+print('Label:\t',data[0].gamestates[1].label)
+print('Graph:\t',data[0].gamestates[1].graph)
+print('Meta: \t',data[0].gamestates[1].metadata)        
 
-print('match:\t',data[1].match_id)
-print('Label:\t',data[1].gamestates[10].label)
-print('Graph:\t',data[1].gamestates[10].graph)
-print('Meta: \t',data[1].gamestates[10].metadata)        
+# print('match:\t',data[0].match_id)
+# print('Label:\t',data[0].gamestates[0].label)
+# print('Graph:\t',data[0].gamestates[0].graph)
+# print('Meta: \t',data[0].gamestates[0].metadata)        
+
+# print('match:\t',data[1].match_id)
+# print('Label:\t',data[1].gamestates[10].label)
+# print('Graph:\t',data[1].gamestates[10].graph)
+# print('Meta: \t',data[1].gamestates[10].metadata)        
     
