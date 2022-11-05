1a25261b911b7fa78b65f9d23106d7ca570d43c2
BrianLo <66210536+scs2007022@users.noreply.github.com>
2022-11-05 09:10:51 +0800

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,49 +1,55 @@
 from statsbombpy import sb
 import pandas as pd
+import multiprocessing as mp
+import pickle
+import os
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.width', None)
 
-heads=100
-# pd.set_option('large_repr','info')
-
-# df_comp = sb.competitions()[['competition_id','competition_name','season_id','season_name']]
-# print(df_comp)
-
-# df_EURO = sb.matches(competition_id=55,season_id=43)
+# heads=100
+df_EURO = sb.matches(competition_id=55,season_id=43)
 # print(df_EURO.columns)
 
-# print('-'*100)
+print(df_EURO[['match_id','match_date','home_team','away_team','home_score','away_score','competition_stage','stadium']])
 
-# df_EURO_info = df_EURO#[['match_id','competition_stage','home_team','away_team','home_score','away_score']]
-# print(df_EURO_info['match_status_360'])
+DataLocation = r'C:\Users\scs20\OneDrive - HKUST Connect\2022-23_Fall\COMP4222\Project\comp4222-project\DataDownload'
 
-Match = sb.events(match_id=3788765)
-# print(Match.columns)
-print(
-    # Match[['id','team','player','period','timestamp','location','possession','play_pattern','type','duration','ball_receipt_outcome','pass_recipient','shot_outcome']].sort_values(['possession','period','timestamp']).head(heads)
-    Match[['id','team','player','period','timestamp','location','possession','play_pattern','type','pass_recipient']].sort_values(['possession','period','timestamp']).head(heads)
-)
-Frame = sb.frames(match_id=3788765)
-# print(Frame.columns)
-print(Frame)
+from DataParsing import DataParsing 
 
-from DataParsing import DataParsing
+def mp_construct(match_id):
+    e = pd.read_pickle(os.path.join(DataLocation,f'{match_id}_events.pkl'))
+    f = pd.read_pickle(os.path.join(DataLocation,f'{match_id}_frames.pkl'))
+    m = DataParsing.Match(match_id,match_df=df_EURO,event_df=e,frame_df=f)
+    return m
+def parsing():
+    print('start')
+    print(len(df_EURO['match_id']))
+    with mp.Pool() as pool:
+        Matches = pool.map(mp_construct,df_EURO['match_id'])
+    # Matches.get()
+    with open("data_2.pickle",'wb') as f:
+        pickle.dump(Matches,f,protocol=pickle.HIGHEST_PROTOCOL)
+    print(Matches)
+    print('done')
 
-# for event_id in Match['id']:
-g=DataParsing.GameState(
-    event_id='e42b7a9f-694c-4ff3-ab08-3455ad35f6e3',
-    Match=Match,
-    Frame=Frame
-    )
+# if __name__=='__main__':
+
+import pickle,time
+st = time.time()
+# objects = []
+with (open('data_2.pickle','rb')) as openfile:  # read the parsed data
+    print('reading')
+    while True:
+        try:
+            data=pickle.load(openfile)
+        except EOFError:
+            break
+et = time.time()
+print(et-st) #loading time
+
+print('match:\t',data[0].match_id)
+print('Label:\t',data[0].gamestates[0].label)
+print('Graph:\t',data[0].gamestates[0].graph)
+print('Meta: \t',data[0].gamestates[0].metadata)        
     
-print(g.period_timestamp)
-print(g.actor)
-print(g.teammate)
-print(g.enemy)
-
-
-# EURO_final = sb.events(match_id=3795506)
-# print(EURO_final.columns)
-# print(EURO_final.shape)
-# print(EURO_final[['index','player','timestamp','duration','type']].sort_values(by='timestamp',ascending=False))
