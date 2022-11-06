ff67eb772a3e449ee51d43b2437ff3cc538d7549
unknown <113531839+Lu-Zetian@users.noreply.github.com>
2022-11-06 16:31:40 +0800

    Update train.py

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from DataParsing.DataParsing import Match,GameState
 
 # Hyper parameters
 learning_rate = 1e-4
-epochs = 10
+epochs = 1
 batch_size = 32
 weight_decay = 1e-5
 model_path = ""
@@ -15,7 +15,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 DataLocation = os.path.dirname(os.path.abspath(__file__))
 
 
-def load_training_data(filename):
+def load_data(filename):
     st = time.time()
     sample = os.path.join(DataLocation, filename)
     with (open(sample,'rb')) as openfile:  # read the parsed data
@@ -31,18 +31,43 @@ def load_training_data(filename):
     return data
 
 
-def train(model, train_dataloader, loss_fn, optimizer):
+def label_converter(label):
+    return label
+
+
+def meta_data_converter(meta_data):
+    return meta_data
+
+
+def train(model, data, loss_fn, optimizer):
     model.train()
     # TODO: single training loop
-    hn, cn = model.init(device)
-    for batch_idx, (input, label) in train_dataloader:
-        input, label = input.to(device), label.to(device)
-        optimizer.zero_grad()
-        output, hn, cn = model(input, hn, cn)
-        hn, cn = hn.detach(), cn.detach()
-        loss = loss_fn(output, label)
-        loss.backward()
-        optimizer.step()
+    for i in range(len(data)):
+        hn, cn = model.init(device)
+        for j in range(len(data[i].gamestates)):
+            gamestate = data[i].gamestates[j]
+            try:
+                graph_data, meta_data, label = gamestate.graph, gamestate.metadata, gamestate.label
+            except:
+                continue
+            label = label_converter(label)
+            meta_data = meta_data_converter(meta_data)
+            # graph_data, meta_data, label = graph_data.to(device), meta_data.to(device), label.to(device)
+            print(graph_data, meta_data, label, sep="\n")
+            # print(graph_data.x)
+            # print(label, end=" ")
+            # print(j)
+            break
+            # convert label and meta_data
+        
+    # for batch_idx, (input, label) in train_dataloader:
+    #     input, label = input.to(device), label.to(device)
+    #     optimizer.zero_grad()
+    #     output, hn, cn = model(input, hn, cn)
+    #     hn, cn = hn.detach(), cn.detach()
+    #     loss = loss_fn(output, label)
+    #     loss.backward()
+    #     optimizer.step()
     
 
 def eval(model, test_dataloader):
@@ -62,10 +87,10 @@ def main():
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
     
-    data = load_training_data("sample_data.pkl")
+    data = load_data("sample_data.pkl")
     
-    # for epoch in range(epochs):
-    #     train(model, train_dataloader, loss_fn, optimizer)
+    for epoch in range(epochs):
+        train(model, data, loss_fn, optimizer)
 
 
 if __name__ == "__main__":
