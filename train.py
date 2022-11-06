4696ed13d12dbfb8a04e4b0e916aae02b315fc77
unknown <113531839+Lu-Zetian@users.noreply.github.com>
2022-11-06 13:20:34 +0800

    add load training data

--- train.py
+++ train.py
@@ -1,13 +1,34 @@
+import os
+import pickle, time
 import torch
 import torch.nn as nn
 from sports_gnn.model import SportsGNN
+from DataParsing.DataParsing import Match,GameState
 
 # Hyper parameters
 learning_rate = 1e-4
 epochs = 10
 batch_size = 32
 weight_decay = 1e-5
+model_path = ""
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+DataLocation = os.path.dirname(os.path.abspath(__file__))
+
+
+def load_training_data(filename):
+    st = time.time()
+    sample = os.path.join(DataLocation, filename)
+    with (open(sample,'rb')) as openfile:  # read the parsed data
+        print('reading')
+        while True:
+            try:
+                data=pickle.load(openfile)
+            except EOFError:
+                break
+        print('loaded')
+    et = time.time()
+    print('Reading time:\t',et-st) # loading time
+    return data
 
 
 def train(model, train_dataloader, loss_fn, optimizer):
@@ -41,10 +62,10 @@ def main():
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
     
-    # TODO: get dataloader
+    data = load_training_data("sample_data.pkl")
     
-    for epoch in range(epochs):
-        train(model, train_dataloader, loss_fn, optimizer)
+    # for epoch in range(epochs):
+    #     train(model, train_dataloader, loss_fn, optimizer)
 
 
 if __name__ == "__main__":
